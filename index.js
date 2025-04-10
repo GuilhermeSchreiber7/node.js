74b63b2c51c5aa7e8bb407cf179cb9597005cfb0
GuilhermeSchreiber7 <guilhermeschreiber2007@gmail.com>
2025-04-10 13:20:55 -0300

    FEAT: implements basic HTTP server with routes for pages and API

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
- res.writeHead(200, { 'Content-type': 'text/plain'});
- res.end('Hello world \n');   
+    const url = req.url;
+    
+    if(url === '/'){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('initial page');
+    } else if(url === '/about'){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('page about simple server');
+    } else if(url === '/api') {
+        res.writeHead(200, {'content-type': 'application/json'});
+        res.end(JSON.stringify({name: 'Guilherme', linguage: 'JS'}));
+    } else {
+        res.writeHead(404, {'content-type': 'text/plain'});
+        res.end('page not found');
+    }
 });
 
 server.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000");
-});
+    console.log('Server running at http://localhost:3000/');
+    console.log('To stop the server, press Ctrl + C');
+})
+
